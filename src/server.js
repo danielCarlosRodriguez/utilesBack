71b3221a21dc68259561668b90ae5382d6ea4748
danielCarlosRodriguez <danielcarlosrodriguez@gmail.com>
2026-01-31 07:58:38 -0300

    se agregó 'http://localhost:5175', a allowedOrigins

--- src/server.js
+++ src/server.js
@@ -33,6 +33,7 @@ const corsOptions = {
       'http://localhost:3000',
       'http://localhost:5173',
       'http://localhost:5174',
+      'http://localhost:5175',
       'http://127.0.0.1:5173',
       'http://127.0.0.1:5174',
       'https://utiles-ya.netlify.app',
